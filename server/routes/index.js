fdfc7ed91a6d33d9de961a0b48d42173bdb36913
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-07-14 12:53:52 -0400

    fixed some .ts files

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,6 @@ exports.default = router;
 const index_1 = require("../controllers/index");
 router.get("/", index_1.DisplayHomePage);
 router.get("/home", index_1.DisplayHomePage);
-router.get("/new", index_1.DisplayNewPage);
-router.get("/recent", index_1.DisplayRecentPage);
 router.get("/about", index_1.DisplayAboutPage);
 router.get("/new-survey", index_1.DisplaySurveyPage);
 //# sourceMappingURL=index.js.map
