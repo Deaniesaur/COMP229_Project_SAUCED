5a792c70d8b866386552c42398800defec61a208
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-07-14 13:35:16 -0400

    new question type selections are working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,7 +22,7 @@ const questionTypeDiv = `
     value="option2"
   />
   <label class="form-check-label" for="inlineRadio2">
-    LONG ANSWER
+    SHORT ANSWER
   </label>
 </div>
 <div class="form-check form-check-inline">
@@ -46,7 +46,7 @@ const questionTypeDiv = `
     value="option3"
   />
   <label class="form-check-label" for="inlineRadio3">
-    RATING
+    TRUE / FALSE
   </label>
 </div>
 <br />
@@ -76,6 +76,67 @@ const submitSurveyButton = `
           >
             SUBMIT SURVEY
           </button>`;
+
+const multipleChoiceQuestion = `
+<br />
+<br />
+<div class="form-check form-check-inline">
+<input
+  class="form-check-input"
+  type="radio"
+  name="inlineRadioOptions"
+  id="inlineRadio1"
+  value="option1"
+/>
+<label class="form-check-label" for="inlineRadio1">
+  Option 1
+</label>
+</div>
+<div class="form-check form-check-inline">
+<input
+  class="form-check-input"
+  type="radio"
+  name="inlineRadioOptions"
+  id="inlineRadio2"
+  value="option2"
+/>
+<label class="form-check-label" for="inlineRadio2">
+Option 2
+</label>
+</div>
+<div class="form-check form-check-inline">
+<input
+  class="form-check-input"
+  type="radio"
+  name="inlineRadioOptions"
+  id="inlineRadio3"
+  value="option3"
+/>
+<label class="form-check-label" for="inlineRadio3">
+Option 3
+</label>
+</div>
+<div class="form-check form-check-inline">
+<input
+  class="form-check-input"
+  type="radio"
+  name="inlineRadioOptions"
+  id="inlineRadio3"
+  value="option3"
+/>
+<label class="form-check-label" for="inlineRadio3">
+Option 4
+</label>
+</div>
+<br />
+<br />`;
+
+const shortAnswerQuestion = `
+<br />
+<br />
+<input class="form-control" type="text" placeholder="The participants will fill-in this area." disabled>
+<br />
+<br />`;
 
 function addNewQuestionButton() {
   let div = document.createElement("div");
@@ -116,16 +177,60 @@ function chooseNewQuestionType() {
   div.innerHTML = mainQuestionDiv;
   document.getElementById("main-section").appendChild(div);
   $(div).hide().fadeIn(1000);
-  displayQuestionOptions(1);
+
+  let response;
+  let isChecked = false;
+  let options = document.querySelectorAll(".form-check-input"),
+    i;
+  console.log(options);
+  for (i = 0; i < options.length; i++) {
+    if (options[i].checked) response = i;
+  }
+
+  displayQuestionOptions(response);
   document.getElementById("question-type").remove();
   addNewQuestionButton();
-  //this is the first time user clicks add new question
+  //check if this is the first time user clicks add new question
   if (true) displaySubmitButton();
+}
+
+function displayMultipleChoice() {
+  console.log("Add multiple choice question.");
+  let div = document.createElement("div");
+  div.id = "multiple-choice";
+  div.innerHTML = multipleChoiceQuestion;
+  div.className = "text-center";
+  document.getElementById("main-section").appendChild(div);
+  $(div).hide().fadeIn(1000);
+}
+
+function displayShortAnswer() {
+  console.log("Add short answer question.");
+  let div = document.createElement("div");
+  div.id = "short-answer";
+  div.innerHTML = shortAnswerQuestion;
+  div.className = "text-center";
+  document.getElementById("main-section").appendChild(div);
+  $(div).hide().fadeIn(1000);
 }
 
 function displayQuestionOptions(i) {
   switch (i) {
-    case 1:
+    case 0:
       console.log("Option 1 is selected.");
+      displayMultipleChoice();
+      break;
+    case 1:
+      console.log("Option 2 is selected.");
+      displayShortAnswer();
+      break;
+    case 2:
+      console.log("Option 3 is selected.");
+      displayCheckBoxes();
+      break;
+    case 3:
+      console.log("Option 4 is selected");
+      displayTrueFalse();
+      break;
   }
 }
