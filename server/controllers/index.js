7bcd278350611d9c68c4958b1af4f28278a8e6c3
Evilasio Junior <evilasiojunior@hotmail.com>
2021-07-24 13:44:22 -0400

    registration and login

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -23,3 +23,135 @@ function DisplayLoginPage(req, res, next) {
 }
 exports.DisplayLoginPage = DisplayLoginPage;
 //# sourceMappingURL=index.js.map
+
+//**************** Added by Junior - Please check it out! ****************//
+
+exports.displayLoginPage = (req, res, next) => {
+    // check if the user is already logged in
+    if(!req.user)
+    {
+        res.render('auth/login', 
+        {
+           title: "Login",
+           messages: req.flash('loginMessage'),
+           displayName: req.user ? req.user.displayName : '' 
+        })
+    }
+    else
+    {
+        return res.redirect('/');
+    }
+}
+
+exports.processLoginPage = (req, res, next) => {
+    passport.authenticate('local',
+    (err, user, info) => {
+        // server err?
+        if(err)
+        {
+            return next(err);
+        }
+        // is there a user login error?
+        if(!user)
+        {
+            req.flash('loginMessage', 'Authentication Error');
+            return res.redirect('/login');
+        }
+        req.login(user, (err) => {
+            // server error?
+            if(err)
+            {
+                return next(err);
+            }
+
+            const payload = 
+            {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }
+
+            const authToken = jwt.sign(payload, DB.Secret, {
+                expiresIn: 604800 // 1 week
+            });
+
+            // TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Logged in Successfully!', user: {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }, token: authToken});
+
+            return res.redirect('/survey');
+        });
+    })(req, res, next);
+}
+
+exports.displayRegisterPage = (req, res, next) => {
+    // check if the user is not already logged in
+    if(!req.user)
+    {
+        res.render('auth/register',
+        {
+            title: 'Register',
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    }
+    else
+    {
+        return res.redirect('/');
+    }
+}
+
+exports.processRegisterPage = (req, res, next) => {
+    // instantiate a user object
+    let newUser = new User({
+        username: req.body.username,
+        //password: req.body.password
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.password, (err) => {
+        if(err)
+        {
+            console.log("Error: Inserting New User");
+            if(err.name == "UserExistsError")
+            {
+                req.flash(
+                    'registerMessage',
+                    'Registration Error: User Already Exists!'
+                );
+                console.log('Error: User Already Exists!')
+            }
+            return res.render('auth/register',
+            {
+                title: 'Register',
+                messages: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        }
+        else
+        {
+            // if no error exists, then registration is successful
+
+            // redirect the user and authenticate them
+
+            // TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Registered Successfully!'});
+            
+
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/survey')
+            });
+        }
+    });
+}
+
+module.exports.performLogout = (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+}
